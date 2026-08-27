a750bb9cb3edd2a075c298a81c06cd73f9f1bc0a
Ewelina Gren <greewelina@gmail.com>
2026-08-27 19:18:18 +0100

    Introduces the PuzzleData class

--- pa-back/src/main/java/com/pa/puzzle/Cluster.java
+++ pa-back/src/main/java/com/pa/puzzle/Cluster.java
@@ -3,6 +3,8 @@ package com.pa.puzzle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.Shape;
 import java.awt.geom.Area;
 import java.util.Arrays;
@@ -10,6 +12,11 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+/**
+ * Class defining a cluster of puzzle pieces.
+ * @author Ewelina Gren
+ * @version 1.0
+ */
 public class Cluster {
 
     private static final Logger LOG = LoggerFactory.getLogger(Cluster.class);
@@ -18,12 +25,20 @@ public class Cluster {
     private final Set<Piece> pieces;
     private final Area consolidatedShape;
 
+    /**
+     * Creates a new empty {@code Cluster} that doesn't contain any {@code Pieces}.
+     * @param id the Cluster's id
+     */
     public Cluster(int id) {
         this.id = id;
         pieces = new HashSet<>();
         consolidatedShape = new Area();
     }
 
+    /**
+     * Adds a {@code Piece} to the {@code Cluster}.
+     * @param piece a {@code Piece} to be added
+     */
     public void addPiece(Piece piece) {
         if (pieces.contains(piece)) {
             LOG.warn("{} already contains the piece: {}", this, piece);
@@ -35,18 +50,52 @@ public class Cluster {
         LOG.info("{} added to {}.", piece, this);
     }
 
+    /**
+     * Returns an array of all the {@code Pieces} it the {@code Cluster}.
+     * @return an array of {@code Pieces}
+     */
     public Piece[] getPieces() {
         return pieces.toArray(new Piece[0]);
     }
 
+    /**
+     * Checks if the {@code Cluster} contains the given {@code Piece}.
+     * @param piece {@code Piece} to be verified
+     * @return true if the {@code Piece} was found in the {@code Cluster}, false otherwise
+     */
+    public boolean containsPiece(Piece piece) {
+        return pieces.contains(piece);
+    }
+
+    /**
+     * Returns the total number of {@code Pieces} in the {@code Cluster}.
+     * @return the total number of {@code Pieces}
+     */
     public int countPieces() {
         return pieces.size();
     }
 
+    /**
+     * Returns the shape of the {@code Cluster} consisting of all the joined {@code Pieces}.
+     * @return the consolidated {@code Shape} of the {@code Cluster}
+     */
     public Shape getConsolidatedShape() {
         return consolidatedShape;
     }
 
+    /**
+     * Returns the location of the cluster's top left corner.
+     * @return the corner's location as a {@code Point}
+     */
+    public Point getNWCorner() {
+        Rectangle bounds = getConsolidatedShape().getBounds();
+        return new Point(bounds.x, bounds.y);
+    }
+
+    /**
+     * Returns the ordinals of all the {@code Pieces} which share an edge with the {@code Cluster} and therefore can be joined.
+     * @return the ordinals of all the neighbouring {@code Pieces}
+     */
     public int[] getNeighbouringPiecesOrdinals() {
         Set<Integer> neighbouringPieces = new HashSet<>();
         for (Piece piece : pieces) {
---
+++ pa-back/src/main/java/com/pa/puzzle/PuzzleData.java
@@ -0,0 +1,155 @@
+package com.pa.puzzle;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.awt.Image;
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+public class PuzzleData {
+
+    private static final Logger LOG = LoggerFactory.getLogger(PuzzleData.class);
+
+    private Image image;
+    private Piece[][] pieces;
+    private Cluster finalizedCluster;
+    private final List<Cluster> activeClusters;
+    private final Map<Integer, Point> currentPositions;
+
+    public PuzzleData() {
+        this.currentPositions = new HashMap<>();
+        this.activeClusters = new ArrayList<>();
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setPieces(Piece[][] pieces) {
+        this.pieces = pieces;
+        currentPositions.clear();
+        finalizedCluster = new Cluster(-1);
+
+        for (int row = 0; row < countRows(); row++) {
+            if (pieces[row].length != countColumns()) {
+                throw new IllegalArgumentException("PuzzleData class does not supported puzzles with inconsistent number of columns");
+            }
+
+            for (int column = 0; column < countColumns(); column++) {
+                Piece piece = pieces[row][column];
+                currentPositions.put(piece.getOrdinal(), piece.getNWCorner());
+
+                Cluster cluster = new Cluster(piece.getOrdinal());
+                cluster.addPiece(piece);
+                activeClusters.add(cluster);
+            }
+        }
+    }
+
+    public Piece[][] getPieces() {
+        return pieces;
+    }
+
+    public Piece getPiece(int ordinal) {
+        return Arrays.stream(pieces).flatMap(Arrays::stream)
+                .filter(piece -> piece.getOrdinal() == ordinal).findAny().orElse(null);
+    }
+
+    public void setPiecePosition(int pieceOrdinal, int x, int y) {
+        currentPositions.put(pieceOrdinal, new Point(x, y));
+        LOG.debug("Position of piece {} changed to {}x{}.", pieceOrdinal, x, y);
+    }
+
+    public Point getPiecePosition(int ordinal) {
+        return currentPositions.get(ordinal);
+    }
+
+    public Cluster[] getClusters() {
+        List<Cluster> clusters = new ArrayList<>(activeClusters);
+        clusters.add(finalizedCluster);
+        return clusters.toArray(new Cluster[0]);
+    }
+
+    public Cluster[] getActiveClusters() {
+        return activeClusters.toArray(new Cluster[0]);
+    }
+
+    public Cluster getParentCluster(Piece piece) {
+        return Arrays.stream(getClusters()).filter(cluster -> cluster.containsPiece(piece)).findAny().orElse(null);
+    }
+
+    public Point getClusterPosition(Cluster cluster) {
+        Piece[] pieces = cluster.getPieces();
+        int x = Arrays.stream(pieces).map(p -> getPiecePosition(p.getOrdinal()).x).mapToInt(n -> n).min().orElse(0);
+        int y = Arrays.stream(pieces).map(p -> getPiecePosition(p.getOrdinal()).y).mapToInt(n -> n).min().orElse(0);
+        return new Point(x, y);
+    }
+
+    public void changeClusterPosition(Cluster cluster, Point clusterPosition) {
+        Piece[] pieces = cluster.getPieces();
+        Point targetNWCorner = cluster.getNWCorner();
+
+        Point difference = new Point(clusterPosition.x - targetNWCorner.x, clusterPosition.y - targetNWCorner.y);
+        for (Piece piece : pieces) {
+            Point pieceNWCorner = piece.getNWCorner();
+            setPiecePosition(piece.getOrdinal(), pieceNWCorner.x + difference.x , pieceNWCorner.y + difference.y);
+        }
+    }
+
+    public int countRows() {
+        return pieces != null ? pieces.length : 0;
+    }
+
+    public int countColumns() {
+        return pieces != null ? pieces[0].length : 0;
+    }
+
+    public int countPieces() {
+        return countRows() * countColumns();
+    }
+
+    public int countFinalizedPieces() {
+        return finalizedCluster.countPieces();
+    }
+
+    public void mergeClusters(Cluster mainCluster, Cluster clusterToBeMerged) {
+        if (Objects.equals(mainCluster, clusterToBeMerged)) {
+            LOG.warn("Cannot merge a cluster with itself.");
+            return;
+        }
+
+        for (Piece piece : clusterToBeMerged.getPieces()) {
+            mainCluster.addPiece(piece);
+        }
+
+        LOG.debug("{} with {} piece(-s) was merged into {}.", clusterToBeMerged, clusterToBeMerged.countPieces(), mainCluster);
+        removeCluster(clusterToBeMerged);
+
+        Point clusterPosition = getClusterPosition(mainCluster);
+        changeClusterPosition(mainCluster, clusterPosition);
+    }
+
+    public void removeCluster(Cluster cluster) {
+        activeClusters.remove(cluster);
+        LOG.debug("{} removed from data.", cluster);
+    }
+
+    public void finalize(Cluster cluster) {
+        mergeClusters(finalizedCluster, cluster);
+    }
+
+    public boolean isFinalized(Piece piece) {
+        return finalizedCluster.containsPiece(piece);
+    }
+
+}
